08cc51758fd6a4e547ef6c8430369bc353162d32
Selena <selena.k.rodriguez@gmail.com>
2023-06-22 15:55:06 -0500

    Created a ship class with hit and sink funcs

--- src/js files/factories/shipFactory.js
+++ src/js files/factories/shipFactory.js
@@ -1,5 +1,25 @@
-import { ShipTypes } from "../shipTypes";
-
-const ship = (name) => {};
-
-export { ship };
+class Ship {
+    constructor(name, length, coordinates) {
+      this.name = name;
+      this.length = length;
+      this.coordinates = coordinates
+      this.hitCount = 0;
+      this.sunk = false;
+    }
+  
+    hit() {
+      this.hitCount += 1;
+      return { hitCount: this.hitCount };
+    }
+  
+    isSunk() {
+      if (this.hitCount >= this.length) {
+        return (this.sunk = true);
+      } else {
+        return (this.sunk = false);
+      }
+    }
+  }
+  
+  export { Ship };
+  
