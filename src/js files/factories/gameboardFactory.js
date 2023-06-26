89c7c5a0fab62b8887d833c4479bbc86396adc97
Selena <selena.k.rodriguez@gmail.com>
2023-06-26 16:30:23 -0500

    created function for placing a ship in an empty legal cell

--- src/js files/factories/gameboardFactory.js
+++ src/js files/factories/gameboardFactory.js
@@ -2,29 +2,76 @@ import { Ship } from "./shipFactory";
 
 const width = 10;
 const height = 10;
-const rowLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 class GameBoard {
   constructor() {
     this.board = [];
+    this.initialize();
   }
 
   initialize() {
     for (let y = 1; y <= height; y++) {
       let row = [];
-      for (let x = 0; x < width; x++) {
-        row.push({ x: rowLabels[x], y: y, hasShip: false, isShot: false });
+      for (let x = 1; x <= width; x++) {
+        row.push({ x, y, hasShip: false, isShot: false, shipType: null });
       }
       this.board.push(row);
     }
   }
 
-  placeShip(ship) {
-    let shipType = ship;
-    let shipLength = ship.length;
+  placeShip(ship, startX, startY, endX, endY) {
+    let shipType = ship.name;
+    let startCell = this.board[startY - 1][startX - 1];
+    let endCell = this.board[endY - 1][endX - 1];
 
-    // check if space is free, if not return false and prompt
-    // if space is free, return true and place ship
+    // check the ship's direction
+    let direction;
+    if (startX === endX) {
+      direction = "vertical";
+    } else if (startY === endY) {
+      direction = "horizontal";
+    } else {
+      console.log(
+        "Invalid ship placement, please make sure the ship is places horizontally or vertically"
+      );
+      return false;
+    }
+
+    // find the cells between
+    let betweenCells = [];
+    if (direction === "horizontal") {
+      const minY = Math.min(startY, endY);
+      const maxY = Math.max(startY, endY);
+      for (let y = minY; y < maxY; y++) {
+        betweenCells.push(this.board[y - 1][startX - 1]);
+      }
+    } else if (direction === "vertical") {
+      const minX = Math.min(startX, endX);
+      const maxX = Math.max(startX, endX);
+      for (let x = minX; x < maxX; x++) {
+        betweenCells.push(this.board[startY - 1][x - 1]);
+      }
+    }
+
+    // check is cell has a ship already, if not then place it
+    let allCellsEmpty = betweenCells.every((cell) => !cell.hasShip);
+    if (
+      startCell.hasShip === false &&
+      endCell.hasShip === false &&
+      allCellsEmpty
+    ) {
+      return [
+        (startCell.hasShip = true),
+        (startCell.shipType = shipType),
+        (endCell.hasShip = true),
+        (endCell.shipType = shipType),
+        (betweenCells.hasShip = true),
+        (betweenCells.shipType = shipType),
+      ];
+    } else {
+      console.log("Invalid location, place ship on a free cell.");
+      return false;
+    }
   }
 
   receiveAttack(coordinates) {}
